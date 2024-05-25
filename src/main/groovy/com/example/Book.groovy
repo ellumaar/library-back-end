29fab75adcbb5197e376d99c4b0d1a65e84effec
ellumaar <ellu.maar@gmail.com>
2024-05-25 13:55:02 +0300

    add post endpoint

--- src/main/groovy/com/example/Book.groovy
+++ src/main/groovy/com/example/Book.groovy
@@ -1,13 +1,17 @@
 package com.example
 
+import io.micronaut.serde.annotation.Serdeable
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 @Entity
+@Serdeable.Deserializable
+@Serdeable. Serializable
 class Book {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id
 
     String title
--- src/main/groovy/com/example/BookController.groovy
+++ src/main/groovy/com/example/BookController.groovy
@@ -1,7 +1,11 @@
 package com.example
 
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.annotation.Status
+import io.micronaut.http.HttpStatus
 
 @Controller("api/v1/books")
 class BookController {
@@ -15,5 +19,11 @@ class BookController {
     @Get("/")
     List<Book> listAllBooks() {
         bookRepository.findAll()
+    }
+
+    @Post("/")
+    @Status(HttpStatus.CREATED)
+    Book createBook(@Body Book book) {
+        bookRepository.save(book)
     }
 }
